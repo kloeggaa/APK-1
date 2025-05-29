41536881a1a55daa36030fafa74e0e75fdcc22e3
kloeggaa <dkloeckner@pm.me>
2025-05-29 12:02:52 +0200

    Initial commit

---
+++ app/src/main/java/com/example/bertapan/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.bertapan;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button closeButton = findViewById(R.id.closeButton);
+        closeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+} 
